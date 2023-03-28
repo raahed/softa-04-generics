e2624dcf5d56422dd179cd17fdfe5228dfee4a04
Marvin Fette <fettemarvin@gmail.com>
2023-03-28 18:10:38 +0200

    Finish Practis Tasks 1 up to 3

--- src/test/java/ohm/softa/a04/tests/SimpleListTests.java
+++ src/test/java/ohm/softa/a04/tests/SimpleListTests.java
@@ -18,11 +18,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class SimpleListTests {
 
 	private final Logger logger = LogManager.getLogger();
-	private SimpleList testList;
+	private SimpleList<Integer> testList;
 
 	@BeforeEach
 	void setup(){
-		testList = new SimpleListImpl();
+		testList = new SimpleListImpl<Integer>();
 
 		testList.add(1);
 		testList.add(2);
@@ -35,7 +35,7 @@ public class SimpleListTests {
 	void testAddElements(){
 		logger.info("Testing if adding and iterating elements is implemented correctly");
 		int counter = 0;
-		for(Object o : testList){
+		for(Integer o : testList){
 			counter++;
 		}
 		assertEquals(5, counter);
@@ -50,27 +50,24 @@ public class SimpleListTests {
 	@Test
 	void testFilterAnonymousClass(){
 		logger.info("Testing the filter possibilities by filtering for all elements greater 2");
-		SimpleList result = testList.filter(new SimpleFilter() {
+		SimpleList<Integer> result = testList.filter(new SimpleFilter<Integer>() {
 			@Override
-			public boolean include(Object item) {
-				int current = (int)item;
-				return current > 2;
+			public boolean include(Integer item) {
+				return item > 2;
 			}
 		});
 
-		for(Object o : result){
-			int i = (int)o;
-			assertTrue(i > 2);
+		for(Integer o : result){
+			assertTrue(o > 2);
 		}
 	}
 
 	@Test
 	void testFilterLambda(){
 		logger.info("Testing the filter possibilities by filtering for all elements which are dividable by 2");
-		SimpleList result = testList.filter(o -> ((int) o) % 2 == 0);
-		for(Object o : result){
-			int i = (int)o;
-			assertTrue(i % 2 == 0);
+		SimpleList<Integer> result = testList.filter(o -> (o % 2 == 0));
+		for(Integer o : result){
+			assertTrue(o % 2 == 0);
 		}
 	}
 }
